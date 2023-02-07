2f4e763d65ff3f1c218e5cb0c711900f2b2e8656
Christian-DAntonio <113142133+Christian-DAntonio@users.noreply.github.com>
2023-02-07 15:01:15 -0500

    Add files via upload

---
+++ WEB303As02/WEB303As02/js/main.js
@@ -0,0 +1,10 @@
+// WEB303 Assignment 2
+
+$("button").click(function(){
+  $("#div").load(function (responseTxt, statusTxt, xhr) {
+    if (statusTxt == "success")
+      alert("External content loaded successfully!");
+    if (statusTxt == "error")
+      alert("Error:" + xhr.status + ":" + xhr.statusText);
+  });
+});
