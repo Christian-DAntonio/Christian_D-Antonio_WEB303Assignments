39bc4ffd34eb996312cea2eeb45687659d749a3c
Christian-DAntonio <113142133+Christian-DAntonio@users.noreply.github.com>
2023-03-01 19:13:50 -0500

    Update main.js

--- WEB303As02/js/main.js
+++ WEB303As02/js/main.js
@@ -1,10 +1,30 @@
 // WEB303 Assignment 2
 
-$("button").click(function(){
-  $("#div").load(function (responseTxt, statusTxt, xhr) {
-    if (statusTxt == "success")
-      alert("External content loaded successfully!");
-    if (statusTxt == "error")
-      alert("Error:" + xhr.status + ":" + xhr.statusText);
-  });
+$("document").ready(function{
+    let prospect = $("#prospect");
+    let convert = $("#convert");
+    let retain = $("#retain");
+
+    let content = $("#content");
+
+    function getContent(e){
+      content.slideUp(700, ()=>{
+          let fileName = `${e.target.id}.html`;
+
+          let xhr = new XMLHttpRequest();
+          xhr.open('GET',fileName, true);
+
+          xhr.onload = function(){
+
+            content.html(this.responseText);
+            content.slideDown();
+          }
+
+          xhr.send();
+      });
+    }
+
+    prospect.click(getContent);
+    convert.click(getContent);
+    retain.click(getContent);
 });
